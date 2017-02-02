b3e38f78f8288a87e4cd384cd1ae0646af93c536
bbn <bbn@localhost.localdomain>
2017-02-02 09:33:53 +0100

    Initial commit of app

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pm-app',
+    template: `
+        <h1>Angular2: Getting Started, homie</h1>
+    `
+})
+export class AppComponent { }
