6e1c6b502d54ec7a5826d45fc7468408e300a752
ubbn <ubbn@live.com>
2017-02-06 20:04:56 +0100

    Completed product detail page

--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,5 +1,7 @@
+import { Subscription } from 'rxjs/Rx';
+import { ProductService } from './product.service';
 import { ActivatedRoute, Route, Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
@@ -7,16 +9,33 @@ import { IProduct } from './product';
   templateUrl: 'product-detail.component.html'
 })
 
-export class ProductDetailComponent implements OnInit {
+export class ProductDetailComponent implements OnInit, OnDestroy {
   product: IProduct;
+  errorMessage: string;
   pageTitle: string = 'Product detail';
+  private sub: Subscription;
 
-  constructor(private _route: ActivatedRoute, private _router: Router){
+  constructor(private _route: ActivatedRoute,
+    private _router: Router,
+    private _productService: ProductService){
   }
 
   ngOnInit(): void {
-    let id = +this._route.snapshot.params['id'];
-    this.pageTitle += `: ${id}`; 
+    this.sub = this._route.params.subscribe(
+      params => {
+        let id = +params['id'];
+        this.getProduct(id)
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  getProduct(id: number) {
+    this._productService.getProduct(id).subscribe(
+      product => this.product = product,
+      error => this.errorMessage = <any>error);
   }
 
   onBack(): void {
